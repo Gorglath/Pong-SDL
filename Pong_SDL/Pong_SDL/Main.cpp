de621a39b1cbad63ffa6390befe24d1ed67b2232
Idan Dvir <idan8426@gmail.com>
2021-03-25 18:51:21 +0200

    Created empty window

---
+++ Pong_SDL/Pong_SDL/Main.cpp
@@ -0,0 +1,66 @@
+#include <SDL.h>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 420;
+bool quit = false;
+
+void Update();
+void Draw(SDL_Renderer* renderer);
+void Close(SDL_Renderer* renderer, SDL_Window* window);
+bool HandleInput();
+
+int main(int argc, char* args[])
+{
+	//Initialize SDL Window.
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+	window = SDL_CreateWindow("Pong-SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	//Main game loop.
+	while (!quit)
+	{
+		//Check if the player quit.
+		quit = HandleInput();
+
+		//Update objects.
+		Update();
+
+		//Draw objects.
+		Draw(renderer);
+
+	}
+
+	Close(renderer, window);
+
+	return 0;
+}
+bool HandleInput()
+{
+	SDL_Event e;
+	if (SDL_PollEvent(&e) != 0)
+	{
+		if (e.type == SDL_QUIT)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+void Update()
+{
+
+}
+void Draw(SDL_Renderer* renderer)
+{
+	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+}
+void Close(SDL_Renderer* renderer,SDL_Window* window)
+{
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
